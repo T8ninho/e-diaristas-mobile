5f93dac54ae84170705cadd80dc52bbdd97a8c3b
tonipinguim <tonisantos.contato@gmail.com>
2021-10-19 20:53:14 -0300

    enter

--- src/ui/components/data-display/UserInformation/UserInformation.style.tsx
+++ src/ui/components/data-display/UserInformation/UserInformation.style.tsx
@@ -1,4 +1,5 @@
 import styled, { css } from "@emotion/native";
+import { View } from "react-native";
 import { AirbnbRating } from "react-native-ratings";
 import AppTheme from 'ui/themes/app-theme';
 
@@ -7,11 +8,13 @@ export const InformationContainer = styled.View`
     padding-left: ${({ theme }) => theme.spacing(2)};
 `;
 
-export const UserInformationContainer = styled.View`
+export const UserInformationContainer = styled(View, {
+    shouldForwardProp: (prop) => prop !== 'darker'
+})<{ darker: boolean }>`
     flex-flow: row;
     align-items: center;
     padding: ${({ theme }) => theme.spacing(3) + ' ' + theme.spacing(2)};
-    background-color: ${({ theme }) => theme.colors.grey[100]};
+    background-color: ${({ theme, darker }) => theme.colors.grey[darker ? 100 : 50]};
 `;
 
 export const UserName = styled.Text`
